cb45c22a2dacf600c2fee7bee732abb8a0ee5115
yslamgulypirgulyyev <islamguly28@gmail.com>
2023-07-31 11:34:21 +0200

    add cron last day of the month

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 const DAILY_DUTY_NOTIFICATION_TIME = '0 0 4 * * *'; // every day at 4 am
+const LAST_DAY_OF_MONTH = '0 12 28-31 * *' // every last day of a month at 12 am
 
-
-module.exports = {DAILY_DUTY_NOTIFICATION_TIME}
+module.exports = {DAILY_DUTY_NOTIFICATION_TIME,LAST_DAY_OF_MONTH}
