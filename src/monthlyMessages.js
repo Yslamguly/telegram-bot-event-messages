6a21bd21e235036f64274136be453caf5cfa172f
yslamgulypirgulyyev <islamguly28@gmail.com>
2023-08-01 10:42:31 +0200

    added a function that asks to change the schedule every month

---
+++ src/monthlyMessages.js
@@ -0,0 +1,12 @@
+const {LAST_DAY_OF_MONTH} = require('./constants')
+const cron = require("node-cron");
+const CHAT_ID = process.env.CHAT_ID
+
+const askForCsvScheduleFile = (bot) =>{
+    cron.schedule(LAST_DAY_OF_MONTH,()=>{
+        bot.sendMessage(CHAT_ID,'Halo! It is time to update your monthly schedule file. ' +
+            'Please use (/change schedule) command to send me your CSV file. Allah razy bolsyn 😊!')
+    })
+}
+
+module.exports = {askForCsvScheduleFile}
