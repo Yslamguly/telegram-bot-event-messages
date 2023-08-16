7f105768d9cb1b2dcdf5a58297bf21f5b5401fe7
yslamgulypirgulyyev <islamguly28@gmail.com>
2023-08-16 14:01:24 +0200

    change cron time

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const DAILY_DUTY_NOTIFICATION_TIME = '0 0 14 * * *'; // every day at 4 am
+const DAILY_DUTY_NOTIFICATION_TIME = '* * * * *'; // every day at 4 am
 const LAST_DAY_OF_MONTH = '0 12 28-31 * *' // every last day of a month at 12 am
 const WEEKLY_CLEANING_NOTIFICATION_TIME = '* 7 * * 3 *' //every wednesday at 7am
 
