36d4f6f36771017f3d7dda107cd50447e4a7c3d6
yslamgulypirgulyyev <islamguly28@gmail.com>
2023-08-23 15:48:56 +0200

    correct weekly cleaning schedule time

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const DAILY_DUTY_NOTIFICATION_TIME = '0 0 4,18 * * *'; // every day at 6 am
 const LAST_DAY_OF_MONTH = '0 12 28-31 * *' // every last day of a month at 12 am
-const WEEKLY_CLEANING_NOTIFICATION_TIME = '0 40 15 * * 3' //every wednesday at 7am
+const WEEKLY_CLEANING_NOTIFICATION_TIME = '0 50 15 * * 3' //every wednesday at 7am
 
 module.exports = {DAILY_DUTY_NOTIFICATION_TIME,LAST_DAY_OF_MONTH,WEEKLY_CLEANING_NOTIFICATION_TIME}
