4e3927c11b1f924f9413b948be9aae7275f88998
Yslamguly Pirgulyyev <islamguly28@gmail.com>
2023-09-04 16:11:57 +0200

    made changes to the time

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,9 @@
-const DAILY_DUTY_NOTIFICATION_TIME = '0 0 4,18 * * *'; // every day at 6 am
+const DAILY_DUTY_NOTIFICATION_TIME = '0 25 4,18 * * *'; // every day at 6 am
 const LAST_DAY_OF_MONTH = '0 12 28-31 * *' // every last day of a month at 12 am
-const WEEKLY_CLEANING_NOTIFICATION_TIME = '0 50 15 * * 3' //every wednesday at 7am
+<<<<<<< Updated upstream
+const WEEKLY_CLEANING_NOTIFICATION_TIME = '0 0 7 * * 3' //every wednesday at 7am
 const DAILY_READING_POLL_TIME = '0 0 16 * * *'
+=======
+>>>>>>> Stashed changes
 
 module.exports = {DAILY_DUTY_NOTIFICATION_TIME,LAST_DAY_OF_MONTH,WEEKLY_CLEANING_NOTIFICATION_TIME,DAILY_READING_POLL_TIME}
