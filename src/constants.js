1ee12b6d448afa0d5ebfb74a52ecd75da398b424
Yslamguly <92810736+Yslamguly@users.noreply.github.com>
2023-09-04 16:31:59 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,6 @@
 const DAILY_DUTY_NOTIFICATION_TIME = '0 25 4,18 * * *'; // every day at 6 am
 const LAST_DAY_OF_MONTH = '0 12 28-31 * *' // every last day of a month at 12 am
-<<<<<<< Updated upstream
 const WEEKLY_CLEANING_NOTIFICATION_TIME = '0 0 7 * * 3' //every wednesday at 7am
-const DAILY_READING_POLL_TIME = '0 0 16 * * *'
-=======
->>>>>>> Stashed changes
+const DAILY_READING_POLL_TIME = '0 40 16 * * *'
 
 module.exports = {DAILY_DUTY_NOTIFICATION_TIME,LAST_DAY_OF_MONTH,WEEKLY_CLEANING_NOTIFICATION_TIME,DAILY_READING_POLL_TIME}
