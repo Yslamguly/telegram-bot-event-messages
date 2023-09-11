19bb397b831c0dffe4e5e428626868ac2d6b9f70
yslamgulypirgulyyev <islamguly28@gmail.com>
2023-09-11 11:23:14 +0200

    correct chat ud

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const DAILY_DUTY_NOTIFICATION_TIME = '0 25 4,18 * * *'; // every day at 6 am
 const LAST_DAY_OF_MONTH = '0 12 28-31 * *' // every last day of a month at 12 am
 const WEEKLY_CLEANING_NOTIFICATION_TIME = '0 0 7 * * 3' //every wednesday at 7am
-const DAILY_READING_POLL_TIME = '* * * * *'
+const DAILY_READING_POLL_TIME = '0 0 17 * * *'
 
 module.exports = {DAILY_DUTY_NOTIFICATION_TIME,LAST_DAY_OF_MONTH,WEEKLY_CLEANING_NOTIFICATION_TIME,DAILY_READING_POLL_TIME}
