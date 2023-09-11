3549f952ca346ac83d2f96312fa5769dd199a478
yslamgulypirgulyyev <islamguly28@gmail.com>
2023-09-11 11:17:29 +0200

    test

--- src/dailyMessages.js
+++ src/dailyMessages.js
@@ -2,6 +2,7 @@ const {DAILY_DUTY_NOTIFICATION_TIME,DAILY_READING_POLL_TIME} = require('./consta
 const {buildDailyDutyMessage} = require("./messageBuilder");
 const {createNotificationScheduler,testMessageAfterCSVChange} = require('./utils')
 const FETH_CHAT_ID = process.env.FETH_CHAT_ID
+const MY_CHAT = process.env.MY_CHAT
 const cron = require('node-cron')
 
 const sendDailyDutyNotification = (bot) => {
@@ -16,7 +17,7 @@ const sendDailyBookReadingPoll =  (bot)=>{
         const currentDayOfTheMonth = new Date().getDate().toString();
         const currentMonthOfTheYear = months[new Date().getMonth().toString()]
 
-        bot.sendPoll(FETH_CHAT_ID,`${currentDayOfTheMonth}  ${currentMonthOfTheYear} Feth suresi we 129 Ya Latifu okaldymy?`,['Hawa','Yok'],{is_anonymous:false})
+        bot.sendPoll(MY_CHAT,`${currentDayOfTheMonth}  ${currentMonthOfTheYear} Feth suresi we 129 Ya Latifu okaldymy?`,['Hawa','Yok'],{is_anonymous:false})
             .catch((error)=> console.log(`Send daily book reading poll error: ${error}`))
     },{timezone:'Europe/Budapest'})
 
