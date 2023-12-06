79287e8f6e9cfbd1956aad95b235e8d8797007b0
yslamgulypirgulyyev <islamguly28@gmail.com>
2023-12-06 09:24:45 +0100

    changed time for weekly cleaning

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const DAILY_DUTY_NOTIFICATION_TIME = '0 0 9 * * *'; // every day at 9 am
 const LAST_DAY_OF_MONTH = '0 12 28-31 * *' // every last day of a month at 12 am
-const WEEKLY_CLEANING_NOTIFICATION_TIME = '0 0 9 * * 3' //every wednesday at 9 am
+const WEEKLY_CLEANING_NOTIFICATION_TIME = '0 0 10 * * 3' //every wednesday at 9 am
 const DAILY_READING_POLL_TIME = '0 0 17 * * *'
 
 module.exports = {DAILY_DUTY_NOTIFICATION_TIME,LAST_DAY_OF_MONTH,WEEKLY_CLEANING_NOTIFICATION_TIME,DAILY_READING_POLL_TIME}
