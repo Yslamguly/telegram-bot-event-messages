9686f46de4d006d092f81ae29444fbc432347783
yslamgulypirgulyyev <islamguly28@gmail.com>
2023-12-06 09:24:10 +0100

    changed chat id for weekly cleaning

--- src/weeklyMessages.js
+++ src/weeklyMessages.js
@@ -1,6 +1,6 @@
 const cron = require('node-cron')
 const {WEEKLY_CLEANING_NOTIFICATION_TIME} = require("./constants");
-const FETH_CHAT_ID = process.env.FETH_CHAT_ID
+const CHAT_ID = process.env.CHAT_ID
 
 
 
@@ -11,7 +11,7 @@ const sendNotificationForWeeklyCleaning = (bot) => {
         if (index === names.length) {
             index = 0;
         }
-        bot.sendMessage(FETH_CHAT_ID, `Responsible for cleaning corridor's and kitchen's floor this week is: ${names[index]} 🧼`)
+        bot.sendMessage(CHAT_ID, `Responsible for cleaning corridor's and kitchen's floor this week is: ${names[index]} 🧼`)
             .catch((error) => console.log(`Send weekly cleaning notification error: ${error}`))
         index++;
     },{timezone:'Europe/Budapest'})
